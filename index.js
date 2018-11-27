68e8b9e3c017c25f5682e0d84b341cbb23cde598
Scott Fraser <scottafraser@yahoo.com>
2018-11-26 18:14:09 -0800

    build simple cron job with node

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const cron = require("node-cron");
+const express = require("express");
+const fs = require("fs");
+
+app = express();
+
+cron.schedule('* * * * *', () => {
+    console.log('running a task every minute');
+});
+
+// schedule tasks to be run on the server
+cron.schedule("* * 21 * *", function () {
+    console.log("---------------------");
+    console.log("Running Cron Job");
+    fs.unlink("./error.log", err => {
+        if (err) throw err;
+        console.log("Error file succesfully deleted");
+    });
+});
+
+app.listen("3128", () => {
+    console.log('app listening!');
+});
